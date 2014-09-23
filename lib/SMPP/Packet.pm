30817e2c4a43a5826b4f6fb710217f861a25a995
Alex Radetsky <rad@rad.kiev.ua>
2014-09-23 20:55:45 +0300

    Added encode_optional_parameters(). Need to add decode_optional_parameters.

--- lib/SMPP/Packet.pm
+++ lib/SMPP/Packet.pm
@@ -364,6 +364,102 @@ Readonly my %CHAR_LEN => (
     N => 4,
 );
 
+
+use constant opt_param_tab => { 
+    'dest_addr_subunit' => 0x0005, 
+    'dest_network_type' => 0x0006, 
+    'dest_bearer_type'  => 0x0007,
+    'dest_telematics_id' => 0x0008, 
+
+    'source_addr_subunit' => 0x000d, 
+    'source_network_type' => 0x000e, 
+    'source_bearer_type'  => 0x000f, 
+    'source_telematics_id' => 0x0010, 
+
+    'qos_time_to_live'  => 0x0017,
+    'payload_type'      => 0x0019,
+    'additional_status_info_text' => 0x001d,
+    'receipted_message_id' => 0x001e, 
+
+    'ms_msg_wait_facilities' => 0x0030, 
+    'PVCY_AuthenticationStr' => 0x0101, 
+
+    'privacy_indicator' => 0x0201, 
+    'source_subaddress' => 0x0202, 
+    'dest_subaddress' => 0x0203, 
+    'user_message_reference' => 0x0204,
+    'user_response_code' => 0x0205,
+    'source_port' => 0x020a, 
+    'destination_port' => 0x020b,
+    'sar_msg_ref_num' => 0x020c,
+    'language_indicator' => 0x020d,
+    'sar_total_segments' => 0x020e,
+    'sar_segment_seqnum' => 0x020f,
+    'sc_interface_version' => 0x0210, 
+
+    'CC_CBN' => 0x0301, 
+    'callback_num_pres_ind' => 0x0302, 
+    'callback_num_atag' => 0x0303, 
+    'number_of_messages' => 0x0304, 
+    'callback_num' => 0x0381, 
+
+    'dpf_result' => 0x0420,
+    'set_dpf' => 0x0421, 
+    'ms_availability_status' => 0x0422, 
+    'network_error_code' => 0x0423, 
+    'message_payload' => 0x0424, 
+    'delivery_failure_reason' => 0x0425, 
+    'more_messages_to_send' =>  0x0426, 
+    'message_state' => 0x0427,
+    'congestion_state' => 0x0428, 
+    'ussd_service_op' => 0x0501, 
+
+    'broadcast_channel_indicator' => 0x0600, 
+    'broadcast_content_type' => 0x0601,
+    'broadcast_content_type_info' => 0x0602,
+    'broadcast_message_class' => 0x0603, 
+    'broadcast_rep_num' => 0x0604, 
+    'broadcast_frequency_interval' => 0x0605,
+    'broadcast_area_identifier' => 0x0606, 
+    'broadcast_error_status' => 0x0607, 
+    'broadcast_area_success' => 0x0608, 
+    'broadcast_end_time' => 0x0609, 
+    'broadcast_service_group' => 0x060a, 
+    'billing_identification' => 0x060b, 
+    'source_network_id' =>  0x060d, 
+    'dest_network_id' => 0x060e, 
+    'source_node_id' => 0x060f,
+    'dest_node_id' => 0x0610, 
+    'dest_addr_np_resolution' => 0x0611, 
+    'dest_addr_np_information' => 0x0612, 
+    'dest_addr_np_country' => 0x0613, 
+
+    'display_time' => 0x1201, 
+    'sms_signal' => 0x1203, 
+    'ms_validity' => 0x1204, 
+
+    'IS95A_AlertOnDelivery' =>  0x1304, 
+    'IS95A_LanguageIndicator' => 0x1306, 
+
+    'alert_on_message_delivery' => 0x130c, 
+    'its_reply_type' => 0x1380, 
+    'its_session_info' => 0x1383, 
+
+    'operator_id' => 0x1402,
+    'tariff' => 0x1403, 
+
+    'mcc' => 0x1450, 
+    'mnc' => 0x1451, 
+    'PDC_MessageClass' => 0x1101, 
+    'PDC_PresentationOption' => 0x1102, 
+
+    'PDC_AlertMechanism' => 0x1103, 
+    'PDC_Teleservice' => 0x1104, 
+    'PDC_MultiPartMessage' => 0x1105, 
+    'PDC_PredefinedMsg' => 0x1106
+}; 
+
+
 sub pack_pdu {
     my ($data_ref) = @_;
 
@@ -423,7 +519,25 @@ sub get_body_str {
         warn "Unknown command: $data_ref->{'command'}\n";
         return q{};
     }
-    return pack $template, map { $data_ref->{$_} } @{$attr_seq};
+    my $mandatory_parameters = pack $template, map { $data_ref->{$_} } @{$attr_seq};
+    my $optional_parameters = encode_optional_parameters ($data_ref); 
+
+    return $mandatory_parameters if $optional_parameters eq ''; 
+    return $mandatory_parameters . $optional_parameters; 
+}
+
+sub encode_optional_parameters { 
+    my ($data_ref) = @_; 
+    my $data = '';  
+
+    foreach my $tag ( keys %{ $data_ref }) { 
+        if ( defined ( opt_param_tab->{$tag})) { 
+            my $val = $data_ref->{$tag}; 
+            warn "Added to PDU as optional: ".$tag . "=> ".$val; 
+            $data .= pack 'nna*', opt_param_tab->{$tag}, length($val), $val;
+        }
+    }
+    return $data; 
 }
 
 sub unpack_pdu {
